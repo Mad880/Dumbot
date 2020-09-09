e004d3ce2fad7c04884b9970b1cb99cfafbc7160
Arshia <69681529+Mad880@users.noreply.github.com>
2020-09-09 16:11:58 +0430

    totally didn't add a nsfw command

--- cogs/annoying.py
+++ cogs/annoying.py
@@ -1,5 +1,6 @@
 import discord
 import re
+import aiohttp
 from discord.ext import commands
 """
 this cog is good for some servers but in general you might wanna
@@ -9,7 +10,7 @@ like it that way ok
 class Annoying(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+') # is it a link? 
+        self._link_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+') # is a link? 
         
     async def delete_link(self, message):
         if not message.channel: # self explanatory
@@ -30,6 +31,16 @@ class Annoying(commands.Cog):
         if self.bot.user in message.mentions: # checks if the bot is pinged or not
             await ctx.guild.ban(ctx.author, reason="don't ping me again you fucker") # bans the fUCKING PINGER
             await ctx.send("don't worry guys nothing happend ok cool we cool right yeah cool ok cool bye now") # everything cool right guys?
+
+    @commands.command()
+    @commands.guild_only()
+    # wOOOOPS MY BAD FORGOT TO MAKE A CHECK
+    async def pussy(self, ctx):
+        """totally doens't send a nsfw picture don't worry it's a normal command"""
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://nekobot.xyz/api/image?type=pussy') as r:
+                r = await r.json()
+        await ctx.send(embed=discord.Embed(title="Pussy", color=0xFFC0CB).set_image(url=r['message']))
 
 def setup(bot):
     bot.add_cog(Annoying(bot)) # renamed to annoying because i had more than one idea lol
